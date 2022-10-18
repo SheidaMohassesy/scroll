b2017f2b5826341d23bee6c232bc5a905ead90ae
Sheida Mohassesy <sheida.mohassesi@gmail.com>
2022-10-18 22:55:21 +0200

    Improve the scroll

--- app.js
+++ app.js
@@ -36,7 +36,34 @@ window.addEventListener("scroll", () => {
     else{
         navbar.classList.remove("fixed-nav");
     }
+    if(scrollHeight > 500) {
+        topLink.classList.add("show-link");
+    }
+    else{
+        topLink.classList.remove("show-link");
+    }
 });
 
 // ********** smooth scroll ************
 // select links
+const scrollLinks = document.querySelectorAll(".scroll-link");
+
+scrollLinks.forEach((link) => {
+link.addEventListener("click", (e) => {
+    // prevent default
+e.preventDefault()
+// navigate to specific spot
+const id = e.currentTarget.getAttribute("href").slice(1);
+const element = document.getElementById(id);
+// calculate the heights
+const navHeight = navbar.getBoundingClientRect().height;
+const containerHeight = linksContainer.getBoundingClientRect().height;
+const fixedNav = navbar.classList.contains("fixed-nav");
+let position = element.offsetTop;
+window.scrollTo({
+    left: 0, 
+    top: position,
+        });
+    linksContainer.style.height = 0;
+    });
+});
