e3fdb895c905b0652f859fefe18450764972d08c
Sheida Mohassesy <sheida.mohassesi@gmail.com>
2022-10-19 09:52:22 +0200

    Fix bugs and improve navigate to links

--- app.js
+++ app.js
@@ -49,21 +49,27 @@ window.addEventListener("scroll", () => {
 const scrollLinks = document.querySelectorAll(".scroll-link");
 
 scrollLinks.forEach((link) => {
-link.addEventListener("click", (e) => {
-    // prevent default
-e.preventDefault()
-// navigate to specific spot
-const id = e.currentTarget.getAttribute("href").slice(1);
-const element = document.getElementById(id);
-// calculate the heights
-const navHeight = navbar.getBoundingClientRect().height;
-const containerHeight = linksContainer.getBoundingClientRect().height;
-const fixedNav = navbar.classList.contains("fixed-nav");
-let position = element.offsetTop;
-window.scrollTo({
-    left: 0, 
-    top: position,
+    link.addEventListener("click", (e) => {
+        // prevent default
+        e.preventDefault();
+        // navigate to specific spot
+        const id = e.currentTarget.getAttribute("href").slice(1);
+        const element = document.getElementById(id);
+        // calculate the heights
+        const navHeight = navbar.getBoundingClientRect().height;
+        const containerHeight = linksContainer.getBoundingClientRect().height;
+        const fixedNav = navbar.classList.contains("fixed-nav");
+        let position = element.offsetTop - navHeight;
+        if(!fixedNav) {
+            position = position - navHeight;
+        }
+        if(navHeight > 82) {
+            position = position + containerHeight;
+        }
+        window.scrollTo({
+            left: 0, 
+            top: position,
         });
-    linksContainer.style.height = 0;
+        linksContainer.style.height = 0;
     });
 });
